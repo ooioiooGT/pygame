b0372fa7402fec98e9026a6a0f2e553c02d813a9
Gilber Chen <gilber1002@byui.edu>
2022-10-26 18:09:57 -0600

    10/26/2022

---
+++ Test/classtest.py
@@ -0,0 +1,10 @@
+class test(): 
+    def __init__(self):
+        pass
+    def Testfunction(self):
+        print("in function")
+    def Testf(self):
+        print("test")
+T = test()
+
+T.Testf()
--- testpygame.py
+++ testpygame.py
@@ -1,15 +1,68 @@
-from sys import displayhook
+
+import sys
+from tkinter import CENTER
+from urllib.parse import SplitResult
+from webbrowser import get
 import pygame 
 pygame.init()
-screen = pygame.display.set_mode((800,600))
-time = pygame.time.Clock()
+sh = 1500
+sw = 1500
+FPS = 60
+FPS_clock = pygame.time.Clock()
+
+
+screen = pygame.display.set_mode((sh,sw))
+pygame.display.set_caption("car game")
+
+# bgimage = pygame.image.load("bg.jpeg")  Test the background picture 
+icon = pygame.image.load('/Users/gilber/Documents/code/pygame-main/pygame/vehicle.png')
+pygame.display.set_icon(icon)
+#making the class of the player 
+car_player = pygame.image.load("Pngegg.png")
+class Background(pygame.sprite.Sprite):
+      def __init__(self): 
+          super().__init__()
+          self.bgimage = pygame.image.load("bg.jpeg")
+          self.bgY = 0 
+          self.bgX = 0
+
+      def render(self):
+          screen.blit(self.bgimage, (self.bgX, self.bgY))
+
+ 
+ 
+class Ground(pygame.sprite.Sprite):
+     def __init__(self):
+          super().__init__()
+          self.image = pygame.image.load("bg.jpeg")
+          self.rect =self.image.get_rect(center = (350,350))
+          
+     def render(self):
+          screen.blit(self.image,(self.rect.x, self.rect.y))
+ 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+     
+ 
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__()
+
+
+BG = Background()
+GR =Ground()
 while True:
-     screen.fill((0,0,255))
+     screen.fill((0,0,0))
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                sys.exit()
                pygame.QUIT
-     time.tick(200)
-
+          if event.type == pygame.KEYDOWN:
+               pass
+     
+     # screen.blit(bgimage,(0,0))
+     GR.render()
      
      pygame.display.update()
+     FPS_clock.tick(FPS)
